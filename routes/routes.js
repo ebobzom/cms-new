b458018f57e05a12ab4f5e2520264348b391541c
Elijah Bobzom <ebobzom@gmail.com>
2020-05-11 22:55:26 +0100

    add logout logic

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ import createUser from '../controllers/signup';
 import createUserValidation from '../validators/create-user-validation';
 import loginValidation from '../validators/login-validation';
 import loginUser from '../controllers/login';
+import logout from '../controllers/logout';
 
 function routes(app){
     app.post('/create', createUserValidation, createUser);
     app.post('/login', loginValidation, loginUser);
+    app.post('/logout', logout);
 }
 
 export default routes;
