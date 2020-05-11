96cd8a54d77d3c5aa43fe8f15102a5da2b33a5f8
Elijah Bobzom <ebobzom@gmail.com>
2020-05-12 00:27:18 +0100

    add category logic

--- config/express.limiter.js
+++ config/express.limiter.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 60 minutes
+    windowMs: 60 * 60 * 1000, // 60 minutes
     max: 1000,
     message: "Please try again after an 15min"
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,14 @@
 import createUser from '../controllers/signup';
 import createUserValidation from '../validators/create-user-validation';
 import loginValidation from '../validators/login-validation';
+import categoryValidation from '../validators/category-validation';
 import loginUser from '../controllers/login';
 import logout from '../controllers/logout';
+import category from '../controllers/category'
 
 function routes(app){
     app.post('/create', createUserValidation, createUser);
+    app.post('/category', category);
     app.post('/login', loginValidation, loginUser);
     app.post('/logout', logout);
 }
