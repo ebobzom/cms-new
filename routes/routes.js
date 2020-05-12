cff5297f4fced0c56dd6c61c76bcd6071d575dc8
Elijah Bobzom <ebobzom@gmail.com>
2020-05-12 14:19:38 +0100

    add edit seller logic

--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,13 @@ import loginUser from '../controllers/login';
 import logout from '../controllers/logout';
 import category from '../controllers/category';
 import createSeller from '../controllers/seller';
+import editSeller from '../controllers/edit-seller';
 
 function routes(app){
     app.post('/create', createUserValidation, createUser);
     app.post('/category', categoryValidation, category);
     app.post('/seller', sellerValidation, createSeller);
+    app.put('/seller', sellerValidation, editSeller);
     app.post('/login', loginValidation, loginUser);
     app.post('/logout', logout);
 }
