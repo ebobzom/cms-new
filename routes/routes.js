0b51c1efbcedac3fb6ab97eaf6cc9ac4e6617df6
Elijah Bobzom <ebobzom@gmail.com>
2020-05-12 13:13:00 +0100

    add create seller logic

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,16 @@ import createUser from '../controllers/signup';
 import createUserValidation from '../validators/create-user-validation';
 import loginValidation from '../validators/login-validation';
 import categoryValidation from '../validators/category-validation';
+import sellerValidation from '../validators/sellers-validation';
 import loginUser from '../controllers/login';
 import logout from '../controllers/logout';
-import category from '../controllers/category'
+import category from '../controllers/category';
+import createSeller from '../controllers/seller';
 
 function routes(app){
     app.post('/create', createUserValidation, createUser);
-    app.post('/category', category);
+    app.post('/category', categoryValidation, category);
+    app.post('/seller', sellerValidation, createSeller);
     app.post('/login', loginValidation, loginUser);
     app.post('/logout', logout);
 }
